b6c8f613663bc742032f32652a1a10118f00504a
athena272 <guilhermera272@gmail.com>
2023-04-28 03:39:11 -0300

    Update utils.test.js

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -4,5 +4,18 @@ describe('utils.js', () => {
     it('should return true if number is an integer', () => {
         expect(isValidDecimal(42)).toBe(true)
     })
+
+    it('should return true if number has one decimal place', () => {
+        expect(isValidDecimal(4.2)).toBe(true)
+    })
+
+    it('should return true if number has two decimal places', () => {
+        expect(isValidDecimal(4.22)).toBe(true)
+    })
+
+    it('should return false for numbers with more than two decimal places', () => {
+        expect(isValidDecimal(4.242)).toBe(false)
+        expect(isValidDecimal(4.242541515)).toBe(false)
+    })
 })
 
