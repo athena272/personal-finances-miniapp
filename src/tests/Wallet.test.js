cdf0c8d82f5bb2ef9913390e9fc633965e1de95a
athena272 <guilhermera272@gmail.com>
2023-04-28 18:35:03 -0300

    Update Wallet.test.js

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -7,4 +7,34 @@ describe('Wallet class', () => {
         expect(wallet.balance).toBe(0)
         // expect(wallet.balance).not.toBeGreaterThan(0)
     })
+
+    it('should create an empty wallet with no transactions', () => {
+        const wallet = new Wallet()
+
+        expect(wallet.transactions).toEqual([])
+    })
+
+    it('should save a transaction on a wallet', () => {
+        const wallet = new Wallet()
+        const mockTransaction = 'some transaction'
+        wallet.addTransaction(mockTransaction)
+
+        expect(wallet.transactions).toContain(mockTransaction)
+    })
+
+    it('should update wallet balance when a transaction is saved', () => {
+        const wallet = new Wallet()
+        const mockTransaction = { value: 42 }
+        wallet.addTransaction(mockTransaction)
+
+        expect(wallet.balance).toBe(42)
+    })
+
+    it('should get wallet balance as a number of up to two decimal places', () => {
+        const wallet = new Wallet()
+        const mockTransaction = { value: 4219 }
+        wallet.addTransaction(mockTransaction)
+
+        expect(wallet.getBalance()).toBe(42.19)
+    })
 })
