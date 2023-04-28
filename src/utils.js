b3e2fbee3666355dd199328b175a0cbf0ba5053d
athena272 <guilhermera272@gmail.com>
2023-04-28 01:10:40 -0300

    Delete utils.js

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-exports.isValidDecimal = (number) => {
-    if (Number.isInteger(number)) return true
-    if (number.toFixed(2) === `${number}0`) return true
-    if (number.toFixed(2) === number.toString()) return true
-    return false
-}
