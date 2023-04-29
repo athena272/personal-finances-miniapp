890567da10c479067b2d57203e6ee424d1ad2b30
athena272 <guilhermera272@gmail.com>
2023-04-29 05:39:00 -0300

    Update Wallet.test.js

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -37,4 +37,47 @@ describe('Wallet class', () => {
 
         expect(wallet.getBalance()).toBe(42.19)
     })
+
+    it('should get all the incomes', () => {
+        const wallet = new Wallet()
+        const incomeX = {
+            value: 5031,
+            createdAt: new Date(),
+            getValue: () => 50.31
+        }
+        const incomeY = {
+            value: 8000,
+            createdAt: new Date(),
+            getValue: () => 80
+        }
+        wallet.addTransaction(incomeX)
+        wallet.addTransaction(incomeY)
+
+        const incomes = wallet.getAllIncomes()
+
+        expect(incomes.total).toBe(80 + 50.31)
+        expect(incomes.list).toContain(incomeX, incomeY)
+    })
+
+    it('should get all the incomes and no expenses', () => {
+        const wallet = new Wallet()
+        const income = {
+            value: 5031,
+            createdAt: new Date(),
+            getValue: () => 50.31
+        }
+        const expense = {
+            value: -4000,
+            createdAt: new Date(),
+            getValue: () => -40
+        }
+        wallet.addTransaction(income)
+        wallet.addTransaction(expense)
+
+        const incomes = wallet.getAllIncomes()
+
+        expect(incomes.total).toBe(50.31)
+        expect(incomes.list).toContain(income)
+        expect(incomes.list).not.toContain(expense)
+    })
 })
