9716f135e934c85d98773eb2b97e5edfb08b7a28
athena272 <guilhermera272@gmail.com>
2023-04-30 13:19:04 -0300

    Update Wallet.test.js

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -80,7 +80,98 @@ describe('Wallet class', () => {
         expect(incomes.list).toContain(income)
         expect(incomes.list).not.toContain(expense)
     })
-    
+
+    it('should not include incomes created before start date', () => {
+        const wallet = new Wallet()
+        const startDate = new Date('2022-01-01')
+        const incomeBeforeStartDate = {
+            value: 5031,
+            createdAt: new Date(startDate.getTime() - 1),
+            getValue: () => 50.31
+        }
+        const incomeAfterStartDate = {
+            value: 8000,
+            createdAt: new Date(startDate.getTime() + 1),
+            getValue: () => 80
+        }
+        wallet.addTransaction(incomeAfterStartDate)
+        wallet.addTransaction(incomeBeforeStartDate)
+
+        const incomes = wallet.getAllIncomes(startDate)
+
+        expect(incomes.total).toBe(80)
+        expect(incomes.list).toContain(incomeAfterStartDate)
+        expect(incomes.list).not.toContain(incomeBeforeStartDate)
+    })
+
+    it('should not include incomes created after end date', () => {
+        const wallet = new Wallet()
+        const startDate = new Date('2022-01-01')
+        const endDate = new Date('2022-01-31')
+        const incomeBeforeEndDate = {
+            value: 5031,
+            createdAt: new Date(endDate.getTime() - 1),
+            getValue: () => 50.31
+        }
+        const incomeAfterEndDate = {
+            value: 8000,
+            createdAt: new Date(endDate.getTime() + 1),
+            getValue: () => 80
+        }
+        wallet.addTransaction(incomeBeforeEndDate)
+        wallet.addTransaction(incomeAfterEndDate)
+
+        const incomes = wallet.getAllIncomes(startDate, endDate)
+
+        expect(incomes.total).toBe(50.31)
+        expect(incomes.list).toContain(incomeBeforeEndDate)
+        expect(incomes.list).not.toContain(incomeAfterEndDate)
+    })
+
+    it('should get the all expenses', () => {
+        const wallet = new Wallet()
+        const expenseA = {
+            value: -3500,
+            createdAt: new Date(),
+            getValue: () => -35
+        }
+        const expenseB = {
+            value: -1299,
+            createdAt: new Date(),
+            getValue: () => -12.99
+        }
+        wallet.addTransaction(expenseA)
+        wallet.addTransaction(expenseB)
+
+        const expenses = wallet.getAllExpenses()
+
+        expect(expenses.total).toBe(47.99)
+        expect(expenses.list).toContain(expenseA)
+        expect(expenses.list).toContain(expenseB)
+    })
+
+    it('should get the all expenses and no incomes', () => {
+        const wallet = new Wallet()
+        const income = {
+            value: 5031,
+            createdAt: new Date(),
+            getValue: () => 50.31
+        }
+        const expense = {
+            value: -1299,
+            createdAt: new Date(),
+            getValue: () => -12.99
+        }
+        wallet.addTransaction(income)
+        wallet.addTransaction(expense)
+
+        const expenses = wallet.getAllExpenses()
+
+        expect(expenses.total).toBe(12.99)
+        expect(expenses.list).toContain(expense)
+        expect(expenses.list).not.toContain(income)
+    })
+
     it('should not include expenses created before start date', () => {
         const wallet = new Wallet()
         const startDate = new Date('2022-01-01')
