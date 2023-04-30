6060965ebde922999b1244ec75d827696ca31c72
athena272 <guilhermera272@gmail.com>
2023-04-30 13:32:19 -0300

    Update Wallet.test.js

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,21 +1,35 @@
 const Wallet = require("../scripts/Wallet")
 
 describe('Wallet class', () => {
+    let wallet
+    const mockIncome = {
+        value: 5031,
+        createdAt: new Date(),
+        getValue: () => 50.31
+    }
+    const mockExpense = {
+        value: -1299,
+        createdAt: new Date(),
+        getValue: () => -12.99
+    }
+
+    beforeEach(() => {
+        wallet = new Wallet()
+    })
+
     it('should create an empty wallet with balance 0', () => {
-        const wallet = new Wallet()
 
         expect(wallet.balance).toBe(0)
         // expect(wallet.balance).not.toBeGreaterThan(0)
     })
 
     it('should create an empty wallet with no transactions', () => {
-        const wallet = new Wallet()
 
         expect(wallet.transactions).toEqual([])
     })
 
     it('should save a transaction on a wallet', () => {
-        const wallet = new Wallet()
+
         const mockTransaction = 'some transaction'
         wallet.addTransaction(mockTransaction)
 
@@ -23,7 +37,7 @@ describe('Wallet class', () => {
     })
 
     it('should update wallet balance when a transaction is saved', () => {
-        const wallet = new Wallet()
+
         const mockTransaction = { value: 42 }
         wallet.addTransaction(mockTransaction)
 
@@ -31,7 +45,7 @@ describe('Wallet class', () => {
     })
 
     it('should get wallet balance as a number of up to two decimal places', () => {
-        const wallet = new Wallet()
+
         const mockTransaction = { value: 4219 }
         wallet.addTransaction(mockTransaction)
 
@@ -39,50 +53,30 @@ describe('Wallet class', () => {
     })
 
     it('should get all the incomes', () => {
-        const wallet = new Wallet()
-        const incomeX = {
-            value: 5031,
-            createdAt: new Date(),
-            getValue: () => 50.31
-        }
-        const incomeY = {
-            value: 8000,
-            createdAt: new Date(),
-            getValue: () => 80
-        }
-        wallet.addTransaction(incomeX)
-        wallet.addTransaction(incomeY)
+
+        wallet.addTransaction(mockIncome)
+        wallet.addTransaction(mockIncome)
 
         const incomes = wallet.getAllIncomes()
 
-        expect(incomes.total).toBe(80 + 50.31)
-        expect(incomes.list).toContain(incomeX, incomeY)
+        expect(incomes.total).toBe(50.31 + 50.31)
+        expect(incomes.list).toContain(mockIncome, mockIncome)
     })
 
     it('should get all the incomes and no expenses', () => {
-        const wallet = new Wallet()
-        const income = {
-            value: 5031,
-            createdAt: new Date(),
-            getValue: () => 50.31
-        }
-        const expense = {
-            value: -1299,
-            createdAt: new Date(),
-            getValue: () => -12.99
-        }
-        wallet.addTransaction(income)
-        wallet.addTransaction(expense)
+
+        wallet.addTransaction(mockIncome)
+        wallet.addTransaction(mockExpense)
 
         const incomes = wallet.getAllIncomes()
 
         expect(incomes.total).toBe(50.31)
-        expect(incomes.list).toContain(income)
-        expect(incomes.list).not.toContain(expense)
+        expect(incomes.list).toContain(mockIncome)
+        expect(incomes.list).not.toContain(mockExpense)
     })
 
     it('should not include incomes created before start date', () => {
-        const wallet = new Wallet()
+
         const startDate = new Date('2022-01-01')
         const incomeBeforeStartDate = {
             value: 5031,
@@ -105,7 +99,7 @@ describe('Wallet class', () => {
     })
 
     it('should not include incomes created after end date', () => {
-        const wallet = new Wallet()
+
         const startDate = new Date('2022-01-01')
         const endDate = new Date('2022-01-31')
         const incomeBeforeEndDate = {
@@ -129,51 +123,31 @@ describe('Wallet class', () => {
     })
 
     it('should get the all expenses', () => {
-        const wallet = new Wallet()
-        const expenseA = {
-            value: -3500,
-            createdAt: new Date(),
-            getValue: () => -35
-        }
-        const expenseB = {
-            value: -1299,
-            createdAt: new Date(),
-            getValue: () => -12.99
-        }
-        wallet.addTransaction(expenseA)
-        wallet.addTransaction(expenseB)
+
+        wallet.addTransaction(mockExpense)
+        wallet.addTransaction(mockExpense)
 
         const expenses = wallet.getAllExpenses()
 
-        expect(expenses.total).toBe(47.99)
-        expect(expenses.list).toContain(expenseA)
-        expect(expenses.list).toContain(expenseB)
+        expect(expenses.total).toBe(12.99 + 12.99)
+        expect(expenses.list).toContain(mockExpense)
+        expect(expenses.list).toContain(mockExpense)
     })
 
     it('should get the all expenses and no incomes', () => {
-        const wallet = new Wallet()
-        const income = {
-            value: 5031,
-            createdAt: new Date(),
-            getValue: () => 50.31
-        }
-        const expense = {
-            value: -1299,
-            createdAt: new Date(),
-            getValue: () => -12.99
-        }
-        wallet.addTransaction(income)
-        wallet.addTransaction(expense)
+
+        wallet.addTransaction(mockIncome)
+        wallet.addTransaction(mockExpense)
 
         const expenses = wallet.getAllExpenses()
 
         expect(expenses.total).toBe(12.99)
-        expect(expenses.list).toContain(expense)
-        expect(expenses.list).not.toContain(income)
+        expect(expenses.list).toContain(mockExpense)
+        expect(expenses.list).not.toContain(mockIncome)
     })
 
     it('should not include expenses created before start date', () => {
-        const wallet = new Wallet()
+
         const startDate = new Date('2022-01-01')
         const expenseBeforeStartDate = {
             value: -3500,
@@ -196,7 +170,7 @@ describe('Wallet class', () => {
     })
 
     it('should not include expenses created after end date', () => {
-        const wallet = new Wallet()
+
         const startDate = new Date('2022-01-01')
         const endDate = new Date('2022-01-31')
         const expenseBeforeEndDate = {
